b0e5ac62be298f4429620425cb2a2a9e53aca906
Yueh-Hsun Lin <yuehhsul@usc.edu>
2017-10-18 22:24:50 -0700

    basic bootup of aws server

--- server/aws.c
+++ server/aws.c
@@ -16,9 +16,12 @@
 int main(int argc, char const *argv[])
 {
 	//addrinfo is the address struct
-	struct addrinfo hints, *res;
+	struct addrinfo hints, *servinfo, *p;
 	//sockfd is the socket file descriptor
 	int sockfd, new_fd;
+
+	//For reusing port
+	int yes = 1;
 
 	//Others trying to connect
 	struct sockaddr_storage their_addr;
@@ -31,21 +34,39 @@ int main(int argc, char const *argv[])
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	if(getaddrinfo(NULL, MYPORT, &hints, &res)==-1) {
-		printf("Error on getaddrinfo():Getting address info error\n");
+	if(getaddrinfo(NULL, MYPORT, &hints, &servinfo)!=0) {
+		perror("server getaddrinfo()");
 	}
 
-	//Create a socket
-	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	for(p=servinfo;p!=NULL;p=p->ai_next) {
+		//Create a socket
+		sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 
-	if(sockfd==-1) {
-		printf("Error on socket():Creating socket error\n");
+		if(sockfd==-1) {
+			perror("server socket()");
+			continue; //Continue to the next addrinfo
+		}
+
+		//Bind the socket to a local port (not always necessary)
+		//Kernel (Network OS will bind to a local port if needed)
+		if(bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen)==-1) {
+			perror("server bind()");
+			continue;
+		}
+
+		//Allows reusing/rebinding of the socket port
+		if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		break; //if reached, it means successful in creating a socket and binding
 	}
 
-	//Bind the socket to a local port (not always necessary)
-	//Kernel (Network OS will bind to a local port if needed)
-	if(bind(sockfd, res->ai_addr, res->ai_addrlen)==-1) {
-		printf("Error on bind():Binding error\n");
+	freeaddrinfo(servinfo);
+
+	if(p==NULL) {	//p==NULL means that for loop ended with no successful creating and/or binding
+		printf("Unsuccessful creating and/or binding\n");
 	}
 
 	//Connect to an IP adress on a port
@@ -56,23 +77,19 @@ int main(int argc, char const *argv[])
 	//Start listening
 	listen(sockfd,BACKLOG);
 
+	printf("Server: waiting for connections...\n");
+
 
 	//Start accepting connections from others
-	addr_size = sizeof their_addr;
+/*	addr_size = sizeof their_addr;
 	new_fd = accept(sockfd, (struct sockaddr_storage*)&their_addr,&addr_size);
 
 	if(new_fd==-1) {
-		printf("Error on accept():Accepting error\n");
-	}
+		perror("accept");
+	}*/
 
 
-	//Allows reusing/rebinding of the socket port
-	int yes = 1;
-
-	// if (setsockopt(listener,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
-	// 	perror("setsockopt");
-	// 	exit(1);
-	// } 
+	 
 
 
 
